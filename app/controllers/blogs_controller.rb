2d755ae29833eee1c63c895aaf7b771c97cd57e2
Sinan KILIÇ <sinankilic61@gmail.com>
2019-12-03 15:04:33 +0300

    Refactored before action for portfolio setting

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,11 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
 
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.special_blogs
     @page_title = "My Portfolio Blog"
   end
 
@@ -63,6 +63,15 @@ class BlogsController < ApplicationController
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+    @blog.draft!
+  end
+    redirect_to blogs_url, notice: "Post status has been updated"
   end
 
   private
