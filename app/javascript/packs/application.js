82cd5bba3e8caf5d6aeb3f47e40544324ded5af5
Sinan KILIÇ <sinankilic61@gmail.com>
2020-01-09 18:14:07 +0300

    Updated fix for show page image and added edit button - Integrated cocoon for portfolio items nested form component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ require("channels")
 require("jquery")
 
 
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -17,8 +19,8 @@ require("jquery")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-//= require rails-ujs
 //= require jquery
+//= require rails-ujs
 //= require jquery_ujs
 //= require jquery-ui
 //= require popper
@@ -32,3 +34,5 @@ import "./sinan";
 import "./html.sortable";
 import "./portfolios";
 import "./jquery-ui";
+import "./cocoon";
+
