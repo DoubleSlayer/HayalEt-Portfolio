bb34e7d2eeecfddc3e737a2f8970de4bf6af1593
Sinan KILIÇ <sinankilic61@gmail.com>
2020-01-16 16:58:28 +0300

    Implemented twitter call on tech news page

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -1,10 +1,10 @@
 module SocialTool
   def self.twitter_search
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV.fetch("TWITTER_CONSUMER_KEY")
-      config.consumer_secret     = ENV.fetch("TWITTER_CONSUMER_SECRET")
-      config.access_token        = ENV.fetch("TWITTER_ACCESS_TOKEN")
-      config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
+      config.consumer_key        = Rails.application.credentials.TWITTER_CONSUMER_KEY
+      config.consumer_secret     = Rails.application.credentials.TWITTER_CONSUMER_SECRET
+      config.access_token        = Rails.application.credentials.TWITTER_ACCESS_TOKEN
+      config.access_token_secret = Rails.application.credentials.TWITTER_ACCESS_SECRET
     end
 
     client.search("#rubyonrails", result_type: 'recent').take(6).collect do |tweet|
@@ -12,4 +12,3 @@ module SocialTool
     end
   end
 end
-
